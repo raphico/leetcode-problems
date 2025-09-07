1db4c61049e764cbcc4ad8eea0a828f55f7032dc
Raphael Awoyinfa <RaphicoGit@gmail.com>
2025-09-07 01:12:51 +0100

    Time: 0 ms (100.00%) | Memory: 6 MB (18.46%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,10 +1,28 @@
+// Running time O(n^2)
+
+// func twoSum(nums []int, target int) []int {
+//     for i := range nums {
+//         for j := i + 1; j < len(nums); j++ {
+//             if nums[i] + nums[j] == target {
+//                 return []int{i, j}
+//             }
+//         }
+//     }
+
+//     return nil
+// }
+
+
 func twoSum(nums []int, target int) []int {
-    for i := range nums {
-        for j := i + 1; j < len(nums); j++ {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
+    seen := make(map[int]int)
+
+    for i, num := range nums {
+        complement := target - num
+        if j, ok := seen[complement]; ok {
+            return []int{i, j}
         }
+
+        seen[num] = i
     }
 
     return nil
